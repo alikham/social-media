48ff73653990f9c59afa8f9902e661baef4654b0
Ali Khan <akhan2501@gmail.com>
2020-05-30 11:24:38 +0530

    homescreen tabbar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,49 @@
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget{
-  
-  _HomeScreenState createState()=> _HomeScreenState();
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
-  Widget build(BuildContext context){
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        title: Text(
+          'FRENZY',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).primaryColor,
+            letterSpacing: 10.0,
+          ),
+        ),
+        centerTitle: true,
+        bottom: TabBar(
+          unselectedLabelStyle: TextStyle(fontSize: 18.0),
+          indicatorWeight: 3.0,
+          labelColor: Theme.of(context).primaryColor,
+          labelStyle: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Trending'),
+            Tab(text: 'Latest'),
+          ],
+        ),
+      ),
     );
   }
 }
